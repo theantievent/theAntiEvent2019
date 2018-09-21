9a3aeaa601b4b136956f8a64fe218a2d91e5d358
Catalina <cat2608@gmail.com>
2018-09-21 06:32:50 +0100

    Add 5 more antiasistentes

--- docs/js/attendees.js
+++ docs/js/attendees.js
@@ -4,55 +4,55 @@ const antiAttendees = [
     "avatar": "assets/attendees/gorka.jpg",
     "name": "Gorka",
     "nickname": "@Ercillagorka",
-    "link": "http://twitter.com/ercillagorka"
+    "link": "http://twitter.com/ercillagorka",
   },
   {
     "avatar": "assets/attendees/jumi.jpg",
     "name": "Jumi",
     "nickname": "@akaJumi",
-    "link": "http://twitter.com/akaJumi"
+    "link": "http://twitter.com/akaJumi",
   },
   {
     "avatar": "assets/attendees/apa.jpg",
     "name": "APA",
     "nickname": "@APA42",
-    "link": "http://twitter.com/APA42"
+    "link": "http://twitter.com/APA42",
   },
   {
     "avatar": "assets/attendees/jon.jpg",
     "name": "Jon",
     "nickname": "@jnroji",
-    "link": "http://twitter.com/jnroji"
+    "link": "http://twitter.com/jnroji",
   },
   {
     "avatar": "assets/attendees/viroide.jpg",
     "name": "Viroide",
     "nickname": "@viroide",
-    "link": "http://twitter.com/viroide"
+    "link": "http://twitter.com/viroide",
   },
   {
     "avatar": "assets/attendees/karliky.jpg",
     "name": "Karliky",
     "nickname": "@k4rliky",
-    "link": "http://twitter.com/k4rliky"
+    "link": "http://twitter.com/k4rliky",
   },
   {
     "avatar": "assets/attendees/ladycircus.jpg",
     "name": "Ladycircus",
     "nickname": "@ladycircus",
-    "link": "http://twitter.com/ladycircus"
+    "link": "http://twitter.com/ladycircus",
   },
   {
     "avatar": "assets/attendees/unaizg.jpg",
     "name": "Unai",
     "nickname": "@Usainzg",
-    "link": "http://twitter.com/Usainzg"
+    "link": "http://twitter.com/Usainzg",
   },
   {
     "avatar": "assets/attendees/maria.jpg",
     "name": "Maria",
     "nickname": "@obimiau",
-    "link": "http://twitter.com/obimiau"
+    "link": "http://twitter.com/obimiau",
   },
   {
     "avatar": "assets/attendees/alex.jpeg",
@@ -64,271 +64,301 @@ const antiAttendees = [
     "avatar": "assets/attendees/cronycles.jpg",
     "name": "Cronycles",
     "nickname": "@cronycles",
-    "link": "http://twitter.com/cronycles"
+    "link": "http://twitter.com/cronycles",
   },
   {
     "avatar": "assets/attendees/ibon.png",
     "name": "Ibon",
     "nickname": "@hyperandroid",
-    "link": "http://twitter.com/hyperandroid"
+    "link": "http://twitter.com/hyperandroid",
   },
   {
     "avatar": "assets/attendees/morti.jpg",
     "name": "Imanol",
     "nickname": "@MortimerGoro",
-    "link": "http://twitter.com/MortimerGoro"
+    "link": "http://twitter.com/MortimerGoro",
   },
   {
     "avatar": "assets/attendees/jorge.jpg",
     "name": "Jorge",
     "nickname": "@jorgeatgu",
-    "link": "http://twitter.com/jorgeatgu"
+    "link": "http://twitter.com/jorgeatgu",
   },
   {
     "avatar": "assets/attendees/joseba.jpg",
     "name": "Joseba",
     "nickname": "@_NullPointer_",
-    "link": "http://twitter.com/_NullPointer_"
+    "link": "http://twitter.com/_NullPointer_",
   },
   {
     "avatar": "assets/attendees/di.jpeg",
     "name": "Diana",
     "nickname": "@diana_aceves_",
-    "link": "http://twitter.com/diana_aceves_"
+    "link": "http://twitter.com/diana_aceves_",
   },
   {
     "avatar": "assets/attendees/belen.png",
     "name": "Belén",
     "nickname": "@ladybenko",
-    "link": "http://twitter.com/ladybenko"
+    "link": "http://twitter.com/ladybenko",
   },
   {
     "avatar": "assets/attendees/carmenansio.jpg",
     "name": "Carmen Ansio",
     "nickname": "@carmenansio",
-    "link": "http://twitter.com/carmenansio"
+    "link": "http://twitter.com/carmenansio",
   },
   {
     "avatar": "assets/attendees/torro.jpg",
     "name": "Elena Torro",
     "nickname": "@eletorro",
-    "link": "http://twitter.com/eletorro"
+    "link": "http://twitter.com/eletorro",
   },
   {
     "avatar": "assets/attendees/cataflu.jpg",
     "name": "Cata",
     "nickname": "@cataflu",
-    "link": "http://twitter.com/cataflu"
+    "link": "http://twitter.com/cataflu",
   },
   {
     "avatar": "assets/attendees/koldo.jpg",
     "name": "Koldo",
     "nickname": "@koldo",
-    "link": "http://twitter.com/koldo"
+    "link": "http://twitter.com/koldo",
   },
   {
     "avatar": "assets/attendees/kiko.jpg",
     "name": "Kikobeats",
     "nickname": "@kikobeats",
-    "link": "http://kikobeats.com"
+    "link": "http://kikobeats.com",
   },
   {
     "avatar": "assets/attendees/monbio.jpg",
     "name": "Mónica",
     "nickname": "@monbio",
-    "link": "http://twitter.com/monbio"
+    "link": "http://twitter.com/monbio",
   },
   {
     "avatar": "assets/attendees/ibaiimaz.jpg",
     "name": "Ibai",
     "nickname": "@ibaiimaz",
-    "link": "http://twitter.com/ibaiimaz"
+    "link": "http://twitter.com/ibaiimaz",
   },
   {
     "avatar": "assets/attendees/ibaidev.jpg",
     "name": "Ibai",
     "nickname": "@ibaidev",
-    "link": "https://gitlab.com/ibaidev"
+    "link": "https://gitlab.com/ibaidev",
   },
   {
     "avatar": "assets/attendees/naiara.jpg",
     "name": "Naiara",
     "nickname": "@nabaroa",
-    "link": "http://twitter.com/nabaroa"
+    "link": "http://twitter.com/nabaroa",
   },
   {
     "avatar": "assets/attendees/barrachina.jpg",
     "name": "Jorge",
     "nickname": "@NTKOG",
-    "link": "http://twitter.com/NTKOG"
+    "link": "http://twitter.com/NTKOG",
   },
   {
     "avatar": "assets/attendees/txus.jpeg",
     "name": "Txus Ordorika",
     "nickname": "@txusinho",
-    "link": "http://twitter.com/txusinho"
+    "link": "http://twitter.com/txusinho",
   },
   {
     "avatar": "assets/attendees/replicante.jpg",
     "name": "Elreplicante",
     "nickname": "@elreplicante",
-    "link": "http://twitter.com/elreplicante"
+    "link": "http://twitter.com/elreplicante",
   },
   {
     "avatar": "assets/attendees/uorbe001.png",
     "name": "Unai",
     "nickname": "@uorbe001",
-    "link": "http://twitter.com/uorbe001"
+    "link": "http://twitter.com/uorbe001",
   },
   {
     "avatar": "assets/attendees/sirikon.jpg",
     "name": "Carlos Fdez Llamas",
     "nickname": "@sirikon",
-    "link": "http://mastodon.social/@sirikon"
+    "link": "http://mastodon.social/@sirikon",
   },
   {
     "avatar": "assets/attendees/josebaseba.jpg",
     "name": "Batman 🦇",
     "nickname": "@josebaseba",
-    "link": "http://twitter.com/josebaseba"
+    "link": "http://twitter.com/josebaseba",
   },
   {
     "avatar": "assets/attendees/Nahikariii.jpg",
     "name": "Nahikari",
     "nickname": "@Nahikariii",
-    "link": "http://twitter.com/Nahikariii"
+    "link": "http://twitter.com/Nahikariii",
   },
   {
     "avatar": "assets/attendees/kini.jpg",
     "name": "Kini",
     "nickname": "@kinisoftware",
-    "link": "http://twitter.com/kinisoftware"
+    "link": "http://twitter.com/kinisoftware",
   },
   {
     "avatar": "assets/attendees/chucheria.jpg",
     "name": "Bea",
     "nickname": "@chucheria",
-    "link": "http://twitter.com/chucheria"
+    "link": "http://twitter.com/chucheria",
   },
   {
     "avatar": "assets/attendees/sergixnet.jpg",
     "name": "Sergio",
     "nickname": "@Sergixnet",
-    "link": "http://twitter.com/Sergixnet"
+    "link": "http://twitter.com/Sergixnet",
   },
   {
     "avatar": "assets/attendees/david.jpg",
     "name": "David",
     "nickname": "@DavidGooomez",
-    "link": "http://twitter.com/DavidGooomez"
+    "link": "http://twitter.com/DavidGooomez",
   },
   {
     "avatar": "assets/attendees/iturricha.jpg",
     "name": "Ibai",
     "nickname": "@ibai_iturricha",
-    "link": "http://twitter.com/ibai_iturricha"
+    "link": "http://twitter.com/ibai_iturricha",
   },
   {
     "avatar": "assets/attendees/marta.jpg",
     "name": "Marta M",
     "nickname": "@ladypain",
-    "link": "http://twitter.com/ladypain"
+    "link": "http://twitter.com/ladypain",
   },
   {
     "avatar": "assets/attendees/jonan.jpg",
     "name": "Jonan",
     "nickname": "@jonanone",
-    "link": "http://twitter.com/jonanone"
+    "link": "http://twitter.com/jonanone",
   },
   {
     "avatar": "assets/attendees/esther.jpg",
     "name": "Esther",
     "nickname": "@esloho",
-    "link": "http://twitter.com/esloho"
+    "link": "http://twitter.com/esloho",
   },
   {
     "avatar": "assets/attendees/keldor.jpg",
     "name": "Keldor",
     "nickname": "@keldor88",
-    "link": "http://twitter.com/keldor88"
+    "link": "http://twitter.com/keldor88",
   },
   {
     "avatar": "assets/attendees/oalberdi.jpg",
     "name": "Oihana Alberdi",
     "nickname": "@oalberdi",
-    "link": "http://twitter.com/oalberdi"
+    "link": "http://twitter.com/oalberdi",
   },
   {
     "avatar": "assets/attendees/mikel.jpg",
     "name": "Mikel",
     "nickname": "@ojoven",
-    "link": "http://twitter.com/ojoven"
+    "link": "http://twitter.com/ojoven",
   },
   {
     "avatar": "assets/attendees/raul.jpg",
     "name": "Raul Otaolea",
     "nickname": "@raul_otaolea",
-    "link": "http://twitter.com/raul_otaolea"
+    "link": "http://twitter.com/raul_otaolea",
   },
   {
     "avatar": "assets/attendees/fernando.png",
     "name": "Fernando Fariña",
     "nickname": "@penniath",
-    "link": "http://twitter.com/penniath"
+    "link": "http://twitter.com/penniath",
   },
   {
     "avatar": "assets/attendees/iker.jpg",
     "name": "Iker",
     "nickname": "@wricka69",
-    "link": "http://twitter.com/wricka69"
+    "link": "http://twitter.com/wricka69",
   },
   {
     "avatar": "assets/attendees/ilpapis.jpg",
     "name": "Iñaki",
     "nickname": "@ilPapis",
-    "link": "http://twitter.com/ilPapis"
+    "link": "http://twitter.com/ilPapis",
   },
   {
     "avatar": "assets/attendees/mercedes.jpg",
     "name": "Mercedes",
     "nickname": "@laponyo",
-    "link": "http://twitter.com/laponyo"
+    "link": "http://twitter.com/laponyo",
   },
   {
     "avatar": "assets/attendees/ziraco.jpg",
     "name": "Xabi",
     "nickname": "@ziraco",
-    "link": "http://twitter.com/ziraco"
+    "link": "http://twitter.com/ziraco",
   },
   {
     "avatar": "assets/attendees/aitoraznara.jpg",
     "name": "Aitor",
     "nickname": "@aitoraznara",
-    "link": "http://twitter.com/aitoraznara"
+    "link": "http://twitter.com/aitoraznara",
   },
   {
     "avatar": "assets/attendees/buenapava.jpg",
     "name": "Marina",
     "nickname": "@buenapava",
-    "link": "http://twitter.com/buenapava"
+    "link": "http://twitter.com/buenapava",
   },
   {
     "avatar": "assets/attendees/noxwings.jpg",
     "name": "NoxWings",
     "nickname": "@NoxWings",
-    "link": "http://twitter.com/NoxWings"
+    "link": "http://twitter.com/NoxWings",
   },
   {
     "avatar": "assets/attendees/itortv.jpg",
     "name": "Jordi",
     "nickname": "@Itortv",
-    "link": "http://twitter.com/Itortv"
+    "link": "http://twitter.com/Itortv",
   },
   {
     "avatar": "assets/attendees/ardiluzu.png",
     "name": "Ardiluzu",
     "nickname": "@ardiluzu",
-    "link": "http://twitter.com/ardiluzu"
+    "link": "http://twitter.com/ardiluzu",
+  },
+  {
+    "avatar": "assets/attendees/ivanguardado.jpg",
+    "name": "Iván Guardado",
+    "nickname": "@ivanguardado",
+    "link": "http://twitter.com/ivanguardado",
+  },
+  {
+    "avatar": "assets/attendees/nadiacarreira.jpg",
+    "name": "Nadia",
+    "nickname": "@nadiacarreira",
+    "link": "https://github.com/nadiacarreira",
+  },
+  {
+    "avatar": "assets/attendees/pabloalbizu.jpg",
+    "name": "Pablo",
+    "nickname": "@pabloalbizu",
+    "link": "http://twitter.com/pabloalbizu",
+  },
+  {
+    "avatar": "assets/attendees/garazi_i.jpg",
+    "name": "Garazi",
+    "nickname": "@Garazi_i",
+    "link": "http://twitter.com/Garazi_i",
+  },
+  {
+    "avatar": "assets/attendees/arketipo.jpg",
+    "name": "Diego",
+    "nickname": "@arketipo",
+    "link": "http://twitter.com/arketipo",
   },
 ];
 
